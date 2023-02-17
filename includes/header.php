740a928c3aef42c5dcf25055c17faa816e9f6f1d
Thibault Carrera <thibault.carrera@gmail.com>
2023-02-17 07:25:44 +0100

    html css

---
+++ includes/header.php
@@ -0,0 +1,3 @@
+<header>
+    <div class="logo">To-Do</div>
+</header>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <?php require_once 'includes/head.php'?>
+    <title>To-Do</title>
+</head>
+
+<body>
+    <div class="container">
+        <?php require_once 'includes/header.php'?>
+        <div class="content">
+            <div class="todo-container">
+                <h1>To-Do List</h1>
+                <div class="todo-form"></div>
+                <div class="todo-list"></div>
+            </div>
+        </div>
+        <?php require_once 'includes/footer.php'?>
+    </div>
+
+</body>
+
+</html>
